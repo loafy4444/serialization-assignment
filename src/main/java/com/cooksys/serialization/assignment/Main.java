dbff5fb2f40a8be26f12b2a3a4abfa9f692a4be3
loafy4444 <loafy4444@gmail.com>
2016-08-10 20:09:10 -0500

    Completed
    Nested For loops can use work but no bugs currently.

--- src/main/java/com/cooksys/serialization/assignment/Main.java
+++ src/main/java/com/cooksys/serialization/assignment/Main.java
@@ -15,52 +15,47 @@ import com.cooksys.serialization.assignment.model.Session;
 import com.cooksys.serialization.assignment.model.Student;
 
 public class Main {
-	
+
 	/**
-	 * Creates a {@link Student} object using the given studentContactFile. The
-	 * studentContactFile should be an XML file containing the marshaled form of
-	 * a {@link Contact} object.
+	 * Creates a {@link Student} object using the given studentContactFile. The studentContactFile should be an XML file containing the marshaled form of a {@link Contact} object.
 	 *
 	 * @param studentContactFile
 	 *            the XML file to use
 	 * @param jaxb
 	 *            the JAXB context to use
-	 * @return a {@link Student} object built using the {@link Contact} data in
-	 *         the given file
-	 * @throws JAXBException 
+	 * @return a {@link Student} object built using the {@link Contact} data in the given file
+	 * @throws JAXBException
 	 */
 	public static Student readStudent(File studentContactFile, JAXBContext jaxb) {
 		Contact stCon = null;
-		
-		try{
+
+		try {
 			Unmarshaller unmarshal = jaxb.createUnmarshaller();
 			stCon = (Contact) unmarshal.unmarshal(studentContactFile);
-		}catch(JAXBException e){
+		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
 		Student st = new Student();
 		st.setContact(stCon);
-		
+
 		return st;
 	}
 
 	/**
-	 * Creates a list of {@link Student} objects using the given directory of
-	 * student contact files.
+	 * Creates a list of {@link Student} objects using the given directory of student contact files.
 	 *
 	 * @param studentDirectory
 	 *            the directory of student contact files to use
 	 * @param jaxb
 	 *            the JAXB context to use
-	 * @return a list of {@link Student} objects built using the contact files
-	 *         in the given directory
-	 * @throws JAXBException 
+	 * @return a list of {@link Student} objects built using the contact files in the given directory
+	 * @throws JAXBException
 	 */
 	public static List<Student> readStudents(File studentDirectory, JAXBContext jaxb) {
 		List<Student> sts = new ArrayList<>();
-		
+
 		File[] files = studentDirectory.listFiles();
-		
+
 		for (File file : files) {
 			sts.add(readStudent(file, jaxb));
 		}
@@ -68,65 +63,66 @@ public class Main {
 	}
 
 	/**
-	 * Creates an {@link Instructor} object using the given
-	 * instructorContactFile. The instructorContactFile should be an XML file
-	 * containing the marshaled form of a {@link Contact} object.
+	 * Creates an {@link Instructor} object using the given instructorContactFile. The instructorContactFile should be an XML file containing the marshaled form of a {@link Contact} object.
 	 *
 	 * @param instructorContactFile
 	 *            the XML file to use
 	 * @param jaxb
 	 *            the JAXB context to use
-	 * @return an {@link Instructor} object built using the {@link Contact} data
-	 *         in the given file
-	 * @throws JAXBException 
+	 * @return an {@link Instructor} object built using the {@link Contact} data in the given file
+	 * @throws JAXBException
 	 */
 	public static Instructor readInstructor(File instructorContactFile, JAXBContext jaxb) {
 		Contact instCon = null;
 
-		try{
+		try {
 			Unmarshaller unmarshal = jaxb.createUnmarshaller();
 			instCon = (Contact) unmarshal.unmarshal(instructorContactFile);
-		}catch(JAXBException e){
+		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
 		Instructor inst = new Instructor();
 		inst.setContact(instCon);
-			
+
 		return inst;
 	}
 
 	/**
-	 * Creates a {@link Session} object using the given rootDirectory. A
-	 * {@link Session} root directory is named after the location of the
-	 * {@link Session}, and contains a directory named after the start date of
-	 * the {@link Session}. The start date directory in turn contains a
-	 * directory named `students`, which contains contact files for the students
-	 * in the session. The start date directory also contains an instructor
-	 * contact file named `instructor.xml`.
+	 * Creates a {@link Session} object using the given rootDirectory. A {@link Session} root directory is named after the location of the {@link Session}, and contains a directory named after the start date of the {@link Session}. The start date directory in turn contains a directory named `students`, which contains contact files for the students in the session. The start date directory also contains an instructor contact file named `instructor.xml`.
 	 *
 	 * @param rootDirectory
-	 *            the root directory of the session data, named after the
-	 *            session location
+	 *            the root directory of the session data, named after the session location
 	 * @param jaxb
 	 *            the JAXB context to use
-	 * @return a {@link Session} object built from the data in the given
-	 *         directory
-	 * @throws JAXBException 
+	 * @return a {@link Session} object built from the data in the given directory
+	 * @throws JAXBException
 	 */
 	public static Session readSession(File rootDirectory, JAXBContext jaxb) throws JAXBException {
 		JAXBContext context = JAXBContext.newInstance(Contact.class);
 		Session se = new Session();
-		File[] files = rootDirectory.listFiles();
-		
-		File date = new File("/input/memphis/08-08-2016");
-//		File inst = new File("/input/memphis/08-08-2016/instructor.xml");
-//		File st = new File("/input/memphis/08-08-2016/students");
-		
-		se.setLocation(rootDirectory.getName());
-		se.setStartDate(date.getName());
-		se.setInstructor(readInstructor(new File("input/memphis/08-08-2016/instructor.xml"), context));
-		se.setStudents(readStudents(new File("input/memphis/08-08-2016/students"), context));
-		
+		File[] fs = rootDirectory.listFiles();
+
+		for (File f : fs) {
+			if (f.isDirectory()) {
+				se.setLocation(f.getName());
+				File[] fs1 = f.listFiles();
+				for (File f1 : fs1) {
+					if (f1.isDirectory()) {
+						se.setStartDate(f1.getName());
+						File[] fs2 = f1.listFiles();
+						for (File f2 : fs2) {
+							if (f2.isFile()) {
+								se.setInstructor(readInstructor((f2), context));
+							}
+							if (f2.isDirectory()) {
+								se.setStudents(readStudents((f2), context));
+							}
+						}
+					}
+				}
+			}
+		}
+
 		return se;
 	}
 
@@ -139,42 +135,33 @@ public class Main {
 	 *            the file to which the session is to be written
 	 * @param jaxb
 	 *            the JAXB context to use
-	 * @throws JAXBException 
+	 * @throws JAXBException
 	 */
 	public static void writeSession(Session session, File sessionFile, JAXBContext jaxb) {
-		try{	
+		try {
 			Marshaller marshal = jaxb.createMarshaller();
 			marshal.marshal(session, sessionFile);
-		}catch(JAXBException e){
+		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
 	}
 
 	/**
-	 * Main Method Execution Steps: 1. Configure JAXB for the classes in the
-	 * com.cooksys.serialization.assignment.model package 
-	 * 2. Read a session
-	 * object from the <project-root>/input/memphis/ directory using the methods
-	 * defined above 3. Write the session object to the
-	 * <project-root>/output/session.xml file.
+	 * Main Method Execution Steps: 1. Configure JAXB for the classes in the com.cooksys.serialization.assignment.model package 2. Read a session object from the <project-root>/input/memphis/ directory using the methods defined above 3. Write the session object to the <project-root>/output/session.xml file.
 	 *
-	 * JAXB Annotations and Configuration: You will have to add JAXB annotations
-	 * to the classes in the com.cooksys.serialization.assignment.model package
+	 * JAXB Annotations and Configuration: You will have to add JAXB annotations to the classes in the com.cooksys.serialization.assignment.model package
 	 *
-	 * Check the XML files in the <project-root>/input/ directory to determine
-	 * how to configure the {@link Contact} JAXB annotations
+	 * Check the XML files in the <project-root>/input/ directory to determine how to configure the {@link Contact} JAXB annotations
 	 *
-	 * The {@link Session} object should marshal to look like the following:
-	 * <session location="..." start-date="..."> <instructor> <contact>...
-	 * </contact> </instructor> <students> ...
-	 * <student> <contact>...</contact> </student> ... </students> </session>
-	 * @throws JAXBException 
+	 * The {@link Session} object should marshal to look like the following: <session location="..." start-date="..."> <instructor> <contact>... </contact> </instructor> <students> ... <student> <contact>...</contact> </student> ... </students> </session>
+	 * 
+	 * @throws JAXBException
 	 */
 	public static void main(String[] args) throws JAXBException {
-		try{
-		JAXBContext context = JAXBContext.newInstance(Session.class);
-		writeSession(readSession(new File("input/memphis"), context), new File("output/session.xml"), context);
-		}catch(JAXBException e){
+		try {
+			JAXBContext context = JAXBContext.newInstance(Session.class);
+			writeSession(readSession(new File("input"), context), new File("output/session.xml"), context);
+		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
 	}
--- src/main/java/com/cooksys/serialization/assignment/model/Session.java
+++ src/main/java/com/cooksys/serialization/assignment/model/Session.java
@@ -5,50 +5,55 @@ import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement()
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Session {
-    
+
 	@XmlAttribute
 	private String location;
-    
-    @XmlAttribute(name = "start-date")
-    private String startDate;
-    
-    private Instructor instructor;
-    private List<Student> students;
 
-    public String getLocation() {
-        return location;
-    }
+	@XmlAttribute(name = "start-date")
+	private String startDate;
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
+	private Instructor instructor;
 
-    public String getStartDate() {
-        return startDate;
-    }
+	@XmlElementWrapper
+	@XmlElement(name = "student")
+	private List<Student> students;
 
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
+	public String getLocation() {
+		return location;
+	}
 
-    public Instructor getInstructor() {
-        return instructor;
-    }
+	public void setLocation(String location) {
+		this.location = location;
+	}
 
-    public void setInstructor(Instructor instructor) {
-        this.instructor = instructor;
-    }
+	public String getStartDate() {
+		return startDate;
+	}
 
-    public List<Student> getStudents() {
-        return students;
-    }
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
 
-    public void setStudents(List<Student> students) {
-        this.students = students;
-    }
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
 }
